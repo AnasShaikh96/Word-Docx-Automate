b230148bf8803669cc347a4b97098788b751ce79
Anas Shaikh <shaikhanas246@gmail.com>
2023-05-30 06:23:39 +0530

    initial commit

---
+++ client/src/components/WordReducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import * as fs from "fs";
+import { Document, Packer, Paragraph, TextRun } from "docx";
+
+export default function WordReducer() {
+  return (
+    <div>WordReducer</div>
+  )
+}
