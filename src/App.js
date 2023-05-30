eb4c711c65264fea6dee77bf2f38a3bb24fd5640
Anas Shaikh <shaikhanas246@gmail.com>
2023-05-30 07:01:16 +0530

    file updates

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import WordReducer from "./components/WordReducer";
+
+function App() {
+  return (
+    <>
+      <h1>Below would be example of Word Docx</h1>
+      <div>
+        <WordReducer />
+      </div>
+    </>
+  );
+}
+
+export default App;
